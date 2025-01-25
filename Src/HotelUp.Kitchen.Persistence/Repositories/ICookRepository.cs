90ff6a9b5ecc9c723bd5f27f67f535923a8bd604
Wiaz <wiazhub@gmail.com>
2025-01-25 15:24:27 +0100

    Cook repository

---
+++ Src/HotelUp.Kitchen.Persistence/Repositories/ICookRepository.cs
@@ -0,0 +1,9 @@
+﻿using HotelUp.Kitchen.Persistence.Entities;
+
+namespace HotelUp.Kitchen.Persistence.Repositories;
+
+public interface ICookRepository
+{
+    Task<Cook?> GetByIdAsync(Guid id);
+    Task AddAsync(Cook cook);    
+}
