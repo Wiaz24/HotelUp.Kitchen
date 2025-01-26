e59a131667a5e35eed118e2b0f098ceea9f4066d
Wiaz <wiazhub@gmail.com>
2025-01-26 18:13:56 +0100

    integration test for event handler

--- README.md
+++ README.md
@@ -6,9 +6,17 @@ This service should expose endpoints on port `5006` starting with:
 /api/kitchen/
 ```
 
-## Healthchecks
+## Health checks
 Health status of the service should be available at:
 ```http
 /api/kitchen/_health
 ```
 and should return 200 OK if the service is running, otherwise 503 Service Unavailable.
+
+## Message broker
+This service uses `MassTransit` library to communicate with the message broker. For the purpose of integration with
+another MassTransit service, all published events are declared in the `HotelUp.Kitchen.Services.Events` namespace.
+
+### AMQP Queues
+This service creates queues that consume messages from the following exchanges:
+- `HotelUp.Employee:EmployeeCreatedEvent` from [HotelUp.Employee](https://github.com/Wiaz24/HotelUp.Employee)
---
+++ Tests/HotelUp.Kitchen.Tests.Integration/EventHandlers/EmployeeCreatedEventHandlerTests.cs
@@ -0,0 +1,52 @@
+﻿using HotelUp.Employee.Services.Events;
+using HotelUp.Kitchen.Persistence.Entities;
+using HotelUp.Kitchen.Persistence.Repositories;
+using HotelUp.Kitchen.Services.Events.DTOs;
+using HotelUp.Kitchen.Services.Services;
+using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using Xunit.Abstractions;
+
+namespace HotelUp.Kitchen.Tests.Integration.EventHandlers;
+
+[Collection(nameof(EmployeeCreatedEventHandlerTests))]
+public class EmployeeCreatedEventHandlerTests : IntegrationTestsBase
+{
+    private readonly IBus _bus;
+    
+    public EmployeeCreatedEventHandlerTests(TestWebAppFactory factory, ITestOutputHelper testOutputHelper) 
+        : base(factory, testOutputHelper)
+    {
+        _bus = ServiceProvider.GetRequiredService<IBus>();
+    }
+    
+    private async Task<Cook?> GetCookAsync(Guid employeeId)
+    {
+        using var scope = ServiceProvider.CreateScope();
+        var cookService = scope.ServiceProvider.GetRequiredService<ICookRepository>();
+        return await cookService.GetByIdAsync(employeeId);
+    }
+    
+    [Fact]
+    public async Task HandleAsync_WhenEmployeeIsCook_ShouldCreateCook()
+    {
+        // Arrange
+        var employeeId = Guid.NewGuid();
+        var employeeCreatedEvent = new EmployeeCreatedEvent
+        {
+            Id = employeeId,
+            Role = EmployeeType.Cook
+        };
+        
+        // Act
+        await _bus.Publish(employeeCreatedEvent);
+        await Task.Delay(500);
+        
+        
+        // Assert
+        var cook = await GetCookAsync(employeeId);
+        cook.ShouldNotBeNull();
+        cook.Id.ShouldBe(employeeId);
+    }
+}
